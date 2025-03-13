946dd3bf3a8cdb997c982534c167256d4d82fc76
cynthia-natalie <cynthianatalie573@gmail.com>
2025-03-13 22:02:41 +0700

    initial code

---
+++ README.md
@@ -0,0 +1,21 @@
+# Django Local Library
+
+Follow this tutorial link
+
+https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Server-side/Django
+
+
+## Setup & Installation
+```sh
+   git clone https://github.com/cynthia-natalie/django_locallibrary.git
+   cd django_locallibrary
+   pip install -r requirements.txt
+   python manage.py makemigrations
+   python manage.py migrate
+   python manage.py collectstatic
+   python manage.py test
+   python manage.py createsuperuser
+   python manage.py runserver
+   ```
+
+Access to `http://127.0.0.1:8000` to view the site
---
+++ catalog/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre, Language
+from django.views import generic
+
+# Create your views here.
+def index(request):
+    """View function for home page of site."""
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
+    num_authors = Author.objects.count() 
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_visits': num_visits},
+    )
+
+
+class BookListView(generic.ListView):
+    """Generic class-based view for a list of books."""
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    """Generic class-based detail view for a book."""
+    model = Book
+
+class AuthorListView(generic.ListView):
+    """Generic class-based list view for a list of authors."""
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    """Generic class-based detail view for an author."""
+    model = Author
+
+
+class GenreDetailView(generic.DetailView):
+    """Generic class-based detail view for a genre."""
+    model = Genre
+
+class GenreListView(generic.ListView):
+    """Generic class-based list view for a list of genres."""
+    model = Genre
+    paginate_by = 10
+
+class LanguageDetailView(generic.DetailView):
+    """Generic class-based detail view for a genre."""
+    model = Language
+
+class LanguageListView(generic.ListView):
+    """Generic class-based list view for a list of genres."""
+    model = Language
+    paginate_by = 10
+
+class BookInstanceListView(generic.ListView):
+    """Generic class-based view for a list of books."""
+    model = BookInstance
+    paginate_by = 10
+
+class BookInstanceDetailView(generic.DetailView):
+    """Generic class-based detail view for a book."""
+    model = BookInstance
